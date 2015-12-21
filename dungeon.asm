913ed8c83740371fe0d71a6f8cce4c97bd77e619
= <chaipokoi@users.noreply.github.com>
2015-12-21 12:00:53 +0100

    Finalisation du dessin des murs

--- dungeon.asm
+++ dungeon.asm
@@ -80,47 +80,87 @@ DUNGEON_LOAD_MAP::
   srl a
   add a
   ld b,a
+  ld [_TEMP],a
   ld a,d
   and %00001111
   add a
   ld c,a
+  ld [_TEMP+1],a
   ;Ecriture de la mémoire video en conséquence
   push hl
-  ld hl,_SCRN0+$20
+  ld hl,_SCRN0
+  ld de,$20
   ;Ecriture du mur du haut
-  ld d,0
+  ld c,0
 .dungeon_load_map_draw_wall_top
   ;mur du haut
-  ld [hl],_CELL_WALL_TOP
+  ld [hl],_CELL_WALL_H_OVER
+  push hl
+  add hl,de
+  ld [hl],_CELL_WALL_H
+  pop hl
   inc hl
-  inc d
-  ld a,d
+  inc c
+  ld a,c
   cp b
   jp nz,.dungeon_load_map_draw_wall_top
+;Récupération des valeurs
+  ld a,[_TEMP+1]
+  ld c,a
 ;calcul du nouveau hl pour le mur du bas
-  ld hl,_SCRN0+$20
+  ld hl,_SCRN0
   push hl
   ld d,$20
   ld e,c
   call Multiply
-  ld a,h
-  ld [$C100],a
-  ld a,l
-  ld [$C101],a
   ld d,h
   ld e,l
   pop hl
   add hl,de
-
+;Initialisation des valeurs de dessin
+  ld de,$20
+  ld c,0
 .dungeon_load_map_draw_wall_bot
   ;mur du bas
-  ld [hl],_CELL_WALL_TOP
+  ld [hl],_CELL_WALL_H_OVER
+  push hl
+  add hl,de
+  ld [hl],_CELL_WALL_H
+  pop hl
   inc hl
-  inc d
-  ld a,d
+  inc c
+  ld a,c
   cp b
   jp nz,.dungeon_load_map_draw_wall_bot
-
+;Récupération des valeurs
+  ld a,[_TEMP+1]
+  ld c,a
+;Dessin des murs verticaux
+  ld hl,_SCRN0
+  ld de,$20
+  ld b,0
+.dungeon_load_map_draw_wall_left
+  ld [hl],_CELL_WALL_V
+  add hl,de
+  inc b
+  ld a,b
+  cp c
+  jp nz,.dungeon_load_map_draw_wall_left
+;Calcul de l'emplacement du mur de droite
+  ld hl,_SCRN0
+  ld a,[_TEMP]
+  add l
+  sub 1
+  ld l,a
+;Dessin du mur de droite
+  ld b,0
+.dungeon_load_map_draw_wall_right
+  ld [hl],_CELL_WALL_V
+  add hl,de
+  inc b
+  ld a,b
+  cp c
+  jp nz,.dungeon_load_map_draw_wall_right
 
   pop hl
 
