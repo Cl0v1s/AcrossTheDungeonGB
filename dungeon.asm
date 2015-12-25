d0813e8d14e48287b1b1a5ad68c05d9d9ed48fb1
= <chaipokoi@users.noreply.github.com>
2015-12-25 21:03:17 +0100

    Dessin de la porte de gauche

--- dungeon.asm
+++ dungeon.asm
@@ -86,8 +86,89 @@ DUNGEON_LOAD_MAP::
   add a
   ld c,a
   ld [_TEMP+1],a
-  ;Ecriture de la mémoire video en conséquence
+  ;Sauvegarde de l'emplacement mémoire de la lecture
   push hl
+  ;Dessin des murs
+  call DUNGEON_LOAD_MAP_DRAW_WALLS
+  ;Récupération de l'adresse de récupération des murs
+  pop hl
+  ;Récupération des infos sur les portes
+  ld b,[hl]
+  inc hl
+  ld c,[hl]
+  ;Création des variables de compteur et de boucle
+  ld d,%00000011
+  ld e,0
+.dungeon_load_map_door_for
+  push bc
+  push de
+  ;Traitement des infos sur les portes
+  ld a,b
+  and d
+  cp 0 ;Si les infos valent zéro, alors on ne dessine pas la porte
+  jp z,.dungeon_load_map_door_next
+  ld e,a
+  ld a,c
+  and d
+  ld d,a
+  call Multiply
+  ld a,e;on ne garde que la fin
+  add 2;on ajoute un minimum de 2 qui sera retiré de 1 par le maximum
+  ld b,a
+  call DUNGEON_LOAD_MAP_DRAW_DOOR
+.dungeon_load_map_door_next
+  pop de
+  pop bc
+  inc e
+  sla d;décalage du masque
+  sla d
+  ld a,e
+  cp 4
+  jp nz,.dungeon_load_map_door_for
+
+ret
+
+; DUNGEON_LOAD_MAP_DRAW_DOOR
+; Dessine une porte
+; Paramètres
+; b-> position de la porte
+; e-> index de la porte (0, gauche, 1 haut etc..)
+DUNGEON_LOAD_MAP_DRAW_DOOR::
+  ;Lecture de l'index et saut en conséquence
+  ld a,e
+  cp 0
+  jp z,.dungeon_load_map_draw_door_left
+
+;Dessin de la porte du mur gauche
+.dungeon_load_map_draw_door_left
+  ;on rabote la hauteur pour qu'elle ne dépasse pas la taille de la salle
+  ld a,[_TEMP+1]
+  ld c,a
+  ld a,b
+  cp c
+  jp c,.dungeon_load_map_draw_door_left_jump
+  ;Si la position n'est pas inférieur à la taille du mur on le limite
+  ld a,c
+  sub 2
+  ld b,a
+.dungeon_load_map_draw_door_left_jump;Sinon
+  ld d,b
+  ld e,$20
+  call Multiply
+  ld hl,_SCRN0
+  add hl,de
+  ld [hl],_CELL_DOOR_V
+  ld de,$20
+  add hl,de
+  ld [hl],_CELL_DOOR_V
+
+
+
+ret
+
+
+
+DUNGEON_LOAD_MAP_DRAW_WALLS::
   ld hl,_SCRN0
   ld de,$20
   ;Ecriture du mur du haut
@@ -104,20 +185,16 @@ DUNGEON_LOAD_MAP::
   ld a,c
   cp b
   jp nz,.dungeon_load_map_draw_wall_top
-;Récupération des valeurs
+  ;Récupération des valeurs
   ld a,[_TEMP+1]
   ld c,a
-;calcul du nouveau hl pour le mur du bas
+  ;calcul du nouveau hl pour le mur du bas
   ld hl,_SCRN0
-  push hl
   ld d,$20
   ld e,c
   call Multiply
-  ld d,h
-  ld e,l
-  pop hl
   add hl,de
-;Initialisation des valeurs de dessin
+  ;Initialisation des valeurs de dessin
   ld de,$20
   ld c,0
 .dungeon_load_map_draw_wall_bot
@@ -132,10 +209,10 @@ DUNGEON_LOAD_MAP::
   ld a,c
   cp b
   jp nz,.dungeon_load_map_draw_wall_bot
-;Récupération des valeurs
+  ;Récupération des valeurs
   ld a,[_TEMP+1]
   ld c,a
-;Dessin des murs verticaux
+  ;Dessin des murs verticaux
   ld hl,_SCRN0
   ld de,$20
   ld b,0
@@ -146,13 +223,13 @@ DUNGEON_LOAD_MAP::
   ld a,b
   cp c
   jp nz,.dungeon_load_map_draw_wall_left
-;Calcul de l'emplacement du mur de droite
+  ;Calcul de l'emplacement du mur de droite
   ld hl,_SCRN0
   ld a,[_TEMP]
   add l
   sub 1
   ld l,a
-;Dessin du mur de droite
+  ;Dessin du mur de droite
   ld b,0
 .dungeon_load_map_draw_wall_right
   ld [hl],_CELL_WALL_V
@@ -161,9 +238,6 @@ DUNGEON_LOAD_MAP::
   ld a,b
   cp c
   jp nz,.dungeon_load_map_draw_wall_right
-
-  pop hl
-
 ret
 
 
